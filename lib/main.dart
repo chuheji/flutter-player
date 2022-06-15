5d0909e35a24f744e3cb7d043b5bf3fc2cac5225
liuyx <2849402458@qq.com>
2022-06-15 17:01:50 +0800

    feat: 完成左侧登录抽屉静态页面

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 /*
  * @Author: liuyouxiang<xlfLuminous@163.com>
  * @Date: 2022-06-09 14:44:59
- * @LastEditTime: 2022-06-15 10:50:08
+ * @LastEditTime: 2022-06-15 16:59:05
  * @LastEditors: liuyouxiang<xlfLuminous@163.com>
  * @FilePath: /app/lib/main.dart
  * @Description: 文件描述
@@ -69,25 +69,109 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Drawer Demo'),
       ),
-      body: Center(
+      drawer: Drawer(
+          child: Container(
+        decoration: BoxDecoration(color: Color(0xFFeeeeee)),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(str),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+                margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                padding: EdgeInsets.all(20),
+                child: Flex(
+                  direction: Axis.horizontal,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.person,
+                          color: Colors.pink[100],
+                        ),
+                        Text(
+                          '立即登录',
+                          style: TextStyle(color: Colors.black, fontSize: 14),
+                        ),
+                        Icon(
+                          Icons.keyboard_arrow_right_outlined,
+                        ),
+                      ],
+                    ),
+                    Image.asset(
+                      'images/scan.png',
+                      width: 20,
+                      height: 20,
+                    )
+                  ],
+                )),
+            ListView(
+              padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+              shrinkWrap: true,
+              children: <Widget>[
+                new Container(
+                  decoration:
+                      new BoxDecoration(color: Colors.white, borderRadius: BorderRadius.all(Radius.circular(7.0))),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: Icon(Icons.message),
+                        title: Text('我的消息'),
+                        trailing: Icon(Icons.keyboard_arrow_right_outlined),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.filter_center_focus),
+                        title: Text('云贝中心'),
+                        trailing: Icon(Icons.keyboard_arrow_right_outlined),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            ListView(
+              padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+              shrinkWrap: true,
+              children: <Widget>[
+                new Container(
+                  decoration:
+                      new BoxDecoration(color: Colors.white, borderRadius: BorderRadius.all(Radius.circular(7.0))),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: Icon(Icons.airplane_ticket),
+                        title: Text('云村有票'),
+                        trailing: Icon(Icons.keyboard_arrow_right_outlined),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.shopping_cart_rounded),
+                        title: Text('商城'),
+                        trailing: Icon(Icons.keyboard_arrow_right_outlined),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.queue_music_rounded),
+                        title: Text('Beat交易平台'),
+                        trailing: Icon(Icons.keyboard_arrow_right_outlined),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.computer),
+                        title: Text('游戏专区'),
+                        trailing: Icon(Icons.keyboard_arrow_right_outlined),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.ring_volume),
+                        title: Text('口袋彩铃'),
+                        trailing: Icon(Icons.keyboard_arrow_right_outlined),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )),
     );
   }
 }
